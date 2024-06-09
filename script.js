d7921e67b009c76e97c7ceaafc650f59eedec61e
Pelangi Dwi Mawarni <plngmawarni@gmail.com>
2024-06-09 07:49:11 -0700

    navbarupdate

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ window.onscroll = () => {
         if(top >= offset && top < offset + height){
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a href*=' + id + ' ]').classList.add('active');
+                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active');
             })
         }
     })
